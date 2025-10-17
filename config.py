e37dc27dc59da0882eb71826bdb9a4693e2504ab
Muhammadali Astanakulov <143817886+k1ng-ali@users.noreply.github.com>
2025-10-17 19:13:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 @dataclass(frozen=True)
 class Config:
-    TOKEN: str = os.getenv("BOT_TOKEN", "8312544948:AAEM4lHDUd4Dg3E4L-IR3vmwECXhf7dIBOg")
+    TOKEN: str = os.getenv("BOT_TOKEN", "1111")
     ADMIN_ID: str = os.getenv("ADMIN_ID", "1653169072")
     CHANNEL_ID: str = os.getenv("CHANNEL_ID", "-1002532606911")
     CHANNEL_USERNAME: str = os.getenv("CHANNEL_USERNAME", "dovtalab_bot_official")
